03baf5944322c3eddbace9198b471533e7780f57
sdaphnis <sonnyalbertomedina@gmail.com>
2026-06-05 21:40:17 -0600

    examen final 1

---
+++ FES_materiales/procesos_2026_2/examen_3/# Rough examen 3.py
@@ -0,0 +1,26 @@
+# Rough examen 3
+
+# Pregunta 1: Sobre martingalas discretas, vas a proveer de una estrategia de apuestas basada en un juego de dados, la idea es que parezca que es una martingala, pero que en realidad no lo sea. Para esto, vas a definir una función que simule el juego de dados y una estrategia de apuestas que parezca ser una martingala, pero que tenga una ventaja no tan evidente para el jugador, va a haber dos problemas uno con la adaptabilidad de la estrategia, va a tener que ver un poco hacia el futuro, y además va a tener una ventaja oculta que no sea tan evidente.
+
+# Pregunta 1b: sobre martingalas discretas, vas a pedir que se demuestre que el proceso de poisson compensado es una martingala, y vas a guiar al universitario a traves de los pasos para que usen el Teorema del paro opcional en un problema interesante
+
+# Pregunta 2: Esta va a ser sobre el proces de poisson homogéneo. Le vas a pedir que dados dos procesos de poisson independientes X_1 y X_2, X_1 dado X_1+X_2=n sigue una distribucion binomial, y que es indpependiente del tiempo.
+
+# Pregunta 3: Sobre el proceso de poisson. Este va a ser un problema más aplicado, en donde se pregunten dos probabilidades, primero una sobre un incremento del proceso, donde deba usarse la independencia y estacionariedad de los incrementos. Otra pregunta donde se pregunte algo sobre uno de los tiempos de llegada del proceso, y se deba condicionar para resolverlo
+
+# Pregunta 4: Sobre el movimiento browniano, Vas a pedir que demuestren que una suma de movimientos brownianos dividida por la raiz de 2 es un movimiento browniano, y después vas a pedir calcular el cuantil 2.5 y 97.5 de un incremento del browniano, interpretando ese incremento como el logaritmo de un retorno de una acción financiera, 
+
+# Pregunta 4b:
+a)Simetr´ıa. Sea Bt : t 0 un movimiento Browniano de par´ametro σ2
+.
+Demuestre que para cualesquiera tiempos t s, P Bt Bs 1 2.
+
+b)Movimiento Browniano con tiempo invertido. Sea s 0 fijo y sea
+Bt : t 0 es un movimiento Browniano est´andar. Demuestre que
+el proceso Xt Bs Bs t : t 0, s es un movimiento Browniano
+en el intervalo 0, s . Este es un movimiento Browniano con tiempo
+invertido.
+
+c)Demostrar que el kernel gaussiano cumple la propiedad de semigrupo,
+
+No indiques la respuesta, solo plantea el problema. No indiques cual es el tema especifico, solo plantea el problema.
